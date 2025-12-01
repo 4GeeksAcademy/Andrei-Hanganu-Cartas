7f02784ea6c91481cb8a67d46777086f52ad7920
AndreiHanganu <ganganuandrei462@gmail.com>
2025-12-01 02:30:25 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,141 @@
+import "bootstrap";
+import "./style.css";
+
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+import { startTransition } from "react";
+
+const dealerSum = 0;
+const yourSum = 0;
+
+
+const dealerAceCount = 0;
+const yourAceCount = 0;
+
+const hidden;
+const deck;
+const canHit= true;
+
+window.onload = function(){
+  buildDeck();
+  shuffleDeck();
+  startGame();
+}
+ const buildDeck() {
+  let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let types = ["C", "D", "H", "S"];
+  deck = [];
+  for (let i=0 ; i< types.length; i++){
+    for (let j = 0; j < values.length; j++){
+      deck.push(values[j] + "-" + types[i] );
+    }
+  }
+  console.log(deck);
+ }
+
+ const shuffleDeck(){
+  for(let i=0; i< deck.lenght; i++){
+    let j = Math.floor(Math.random() * deck.length);
+    let temp = deck[i];
+    deck[i] = decl[j];
+    deck[j] = temp;
+  }
+  console.log(deck);
+ }
+ const startGame (){
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+    dealerAceCount += checkAce(hidden);
+    console.log(hidden);
+    console.log(dealerSum);
+    while(dealerSum<17){
+      let cardImg = document.createElement("img");
+      let card = deck.pop();
+      cardImg.src= "./cards/" + card + ".png";
+      dealerSum += getValue(card);
+      dealerAceCount += checkAce(card);
+      document.getElementById("dealer-cards").append(cardImg);
+    }
+    console.log(dealerSum);
+    for (let i=0; i<2; i++){
+       let cardImg = document.createElement("img");
+      let card = deck.pop();
+      cardImg.src= "./cards/" + card + ".png";
+      yourSum += getValue(card);
+      yourAceCount += checkAce(card);
+      document.getElementById("your-cards").append(cardImg);
+    }
+    console.log(yourSum);
+    document.getElementsById("hit").addEventListener("click", hit);
+    document.getElementById("stay").addEventListener("click", stay);
+
+
+ }
+ const hit(){
+  if (!canHit){
+    return;
+  }
+         let cardImg = document.createElement("img");
+      let card = deck.pop();
+      cardImg.src= "./cards/" + card + ".png";
+      yourSum += getValue(card);
+      yourAceCount += checkAce(card);
+      document.getElementById("your-cards").append(cardImg);
+      if (reduceAce(yourSum, yourAceCount) > 21){
+        canHit = false;
+      }
+
+ }
+  const getValue(card){
+    let data = card.split ("-");
+    let value = data[0];
+    if (isNaN(value)){
+      if(value == "A"){
+        return 11;
+      }
+      return 10;
+    }
+
+    return parseInt(value);
+  }
+  const checkAce(card){
+    if (card[0]== "A"){
+      return 1;
+    }
+    return 0;
+  }
+ 
+  const reduceAce(playerSum, playerAceCount){
+    while(playerSum > 21 && playerAceCount > 0){
+      playerSum -= 10;
+      playerAceCount -= 1;
+    }
+    return playerSum;
+  }
+  const stay(){
+    dealerSum= reduceAce(dealerSum, dealerAceCount);
+    yourSum = reduceAce(yourSum, yourAceCount);
+    canHit = false;
+    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+    let message = "";
+    if(yourSum > 21){
+      massage= "You Lose!";
+    }
+    else if (dealerSum > 21){
+      massage = "You win!";
+    }
+    else if (yourSum == dealerSum){
+      message= "Tie!";
+    }
+    else if(yourSum > dealerSum){
+      message= "You Win!";
+
+    }
+    else if(yourSum < dealerSum){
+      message= "You Lose!";
+    }
+    document.getElementById("your-sum").innerText = yourSum;
+    document.getElementById("dealer-sum").innerText = dealerSum;
+    document.getElementById("results").innerText = message;
+  }
